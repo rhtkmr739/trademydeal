7c79908d3f1ba5919daa66ddb41abc62b72d74ff
durgeshpandeysoftware <durgesh@cybuzzsc.com>
2022-07-09 20:08:33 +0530

    Update contactus.blade.php

--- resources/views/livewire/contactus.blade.php
+++ resources/views/livewire/contactus.blade.php
@@ -11,9 +11,9 @@
                         <div class="overlay op7"></div>
                         <div class="container">
                             <div class="section-title center-align big-title">
-                                <h2><span>Our Tariff Plans</span></h2>
+                                <h2><span>Contact-Us</span></h2>
                                 <span class="section-separator"></span>
-                                <div class="breadcrumbs fl-wrap"><a href="/">Home</a><span>Pricing Tables</span></div>
+                                <div class="breadcrumbs fl-wrap"><a href="/">Home</a><span>Contact-Us</span></div>
                             </div>
                         </div>
                         <div class="header-sec-link">
@@ -22,11 +22,96 @@
                     </section>
                     <!--  section  end-->
                     <section   id="sec1" data-scrollax-parent="true">
-                        Contact Us
+                                 <section   id="sec1" data-scrollax-parent="true">
+                        <div class="container">
+                            <!--about-wrap -->
+                            <div class="about-wrap">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <div class="ab_text-title fl-wrap">
+                                            <h3>Get in Touch</h3>
+                                            <span class="section-separator fl-sec-sep"></span>
+                                        </div>
+                                        <!--box-widget-item -->                                       
+                                        <div class="box-widget-item fl-wrap block_box">
+                                            <div class="box-widget">
+                                                <div class="box-widget-content bwc-nopad">
+                                                    <div class="list-author-widget-contacts list-item-widget-contacts bwc-padside">
+                                                        <ul class="no-list-style">
+                                                            <li><span><i class="fal fa-map-marker"></i> Address :</span> <a href="#singleMap" class="custom-scroll-link">A-1,3rd floor,Gulab Bagh,Piller no. 733,Nawada Metro Gate-3,Uttam Nagar,New Delhi,110059</a></li>
+<!--                                                            <li><span><i class="fal fa-phone"></i> Phone :</span> <a href="#">+91 9311781740</a></li>
+-->                                                            
+<li><span><i class="fal fa-envelope"></i> Mail :</span> <a href="#">info@amazefuturetechnology.com</a></li>
+<li><span><i class="fal fa-envelope"></i> Mail :</span> <a href="#">info@trademydeal.com</a></li>
+
+                                                        </ul>
+                                                    </div>
+                                                    <div class="list-widget-social bottom-bcw-box  fl-wrap">
+                                                        <ul class="no-list-style">
+                                                            <li><a href="#" target="_blank" ><i class="fab fa-facebook-f"></i></a></li>
+                                                            <li><a href="#" target="_blank"><i class="fab fa-twitter"></i></a></li>
+                                                            <li><a href="#" target="_blank" ><i class="fab fa-vk"></i></a></li>
+                                                            <li><a href="#" target="_blank" ><i class="fab fa-instagram"></i></a></li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!--box-widget-item end -->  
+                                        <!--box-widget-item -->
+                                      <!--  <div class="box-widget-item fl-wrap" style="margin-top:20px;">
+                                            <div class="banner-wdget fl-wrap">
+                                                <div class="overlay op4"></div>
+                                                <div class="bg"  data-bg="images/bg/1.jpg"></div>
+                                                <div class="banner-wdget-content fl-wrap">
+                                                    <h4>Participate in our loyalty program. Refer a friend and get a discount.</h4>
+                                                    <a href="#" class="color-bg">Read more</a>
+                                                </div>
+                                            </div>
+                                        </div>-->
+                                        <!--box-widget-item end -->                                            
+                                    </div>
+                                    <div class="col-md-8">
+                                        <div class="ab_text">
+                                            <div class="ab_text-title fl-wrap">
+                                                <h3>Drop us a line</h3>
+                                                <span class="section-separator fl-sec-sep"></span>
+                                            </div>
+                                            <p>Our Team are available 24/7 for any types of help and support. if you face any problems/issues or if you have any queries then feel free to contact us.</p>
+                                            <div id="contact-form">
+                                                <div id="message"></div>
+                                                <form  class="custom-form" action="php/contact.php" name="contactform" id="contactform">
+                                                    <fieldset>
+                                                        <label><i class="fal fa-user"></i></label>
+                                                        <input type="text" name="name" id="name" placeholder="Your Name *" value=""/>
+                                                        <div class="clearfix"></div>
+                                                           <label><i class="fal fa-phone"></i>  </label>
+                                                        <input type="text"  name="mobile" id="mobile" placeholder="Mobile No*" value=""/>
+                                                        
+                                                        <label><i class="fal fa-envelope"></i>  </label>
+                                                        <input type="text"  name="email" id="email" placeholder="Email Address*" value=""/>
+                                                        <textarea name="comments"  id="comments" cols="40" rows="3" placeholder="Your Message:"></textarea>
+                                                    </fieldset>
+                                                    <button class="btn float-btn color2-bg" id="submit">Send Message<i class="fal fa-paper-plane"></i></button>
+                                                </form>
+                                            </div>
+                                            <!-- contact form  end--> 
+                                          
+                                        </div>
+                                    </div>
+                                    
+                                </div>
+                                  <br> <br>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3501.4543357231864!2d77.04372221508287!3d28.64611188241248!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d05dbb60db139%3A0x5d6dcf4d522df41e!2sTrade%20My%20Deal!5e0!3m2!1sen!2sin!4v1632774977165!5m2!1sen!2sin" width="1200" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>                                       
+                            </div>
+                            <!-- about-wrap end  --> 
+                        </div>
+                    </section>
                     </section>
                 </div>
                 <!--content end-->
             </div>
+            <p
             <!-- wrapper end-->
 
 @include('common.foot')   

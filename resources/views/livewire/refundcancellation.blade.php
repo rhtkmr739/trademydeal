f177702d4a953b29deb066675d059af30519971c
durgeshpandeysoftware <durgesh@cybuzzsc.com>
2022-07-09 19:58:46 +0530

    Update refundcancellation.blade.php

--- resources/views/livewire/refundcancellation.blade.php
+++ resources/views/livewire/refundcancellation.blade.php
@@ -11,9 +11,9 @@
                         <div class="overlay op7"></div>
                         <div class="container">
                             <div class="section-title center-align big-title">
-                                <h2><span>Our Tariff Plans</span></h2>
+                                <h2><span>Refund and Cancellation</span></h2>
                                 <span class="section-separator"></span>
-                                <div class="breadcrumbs fl-wrap"><a href="/">Home</a><span>Pricing Tables</span></div>
+                                <div class="breadcrumbs fl-wrap"><a href="/">Home</a><span>Refund and Cancellation</span></div>
                             </div>
                         </div>
                         <div class="header-sec-link">
@@ -22,8 +22,32 @@
                     </section>
                     <!--  section  end-->
                     <section   id="sec1" data-scrollax-parent="true">
-                        Refund and Cancellation
-                    </section>
+                        
+                                            <table class="table">
+  <thead class="thead-dark table table-bordered">
+    <tr>
+      <th scope="col">Sr.</th>
+      <th scope="col">Refund & Cancellation</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Once subscribed & the paid services activated on <b><span class="footer-logo-text"><strong style="color:#4DB7FE;">Trade</strong> <strong style="color:#4DB7FE;">My</strong> <strong style="color:#4DB7FE;">Deal</strong></b>, the payment made by the User for activated Paid service is not  refundable under any circumstances
+ and no claim for refund would be entertained.</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td><b><span class="footer-logo-text"><strong style="color:#4DB7FE;">Trade</strong> <strong style="color:#4DB7FE;">My</strong> <strong style="color:#4DB7FE;">Deal</strong></b> neither offers any guarantees for the accuracy, timeliness, authenticity nor advocates any of the 
+content posted by the other Users. </td>
+    </tr>
+     <tr>
+      <th scope="row">3</th>
+      <td>All the services are on a best effort basis. </td>
+    </tr>
+ 
+  </tbody>
+</table>               </section>
                 </div>
                 <!--content end-->
             </div>

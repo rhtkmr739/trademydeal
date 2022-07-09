2965a7bf7a63751ba7e86e493ffe2cede260ea7c
durgeshpandeysoftware <durgesh@cybuzzsc.com>
2022-07-09 19:57:06 +0530

    Update termsconditions.blade.php

--- resources/views/livewire/termsconditions.blade.php
+++ resources/views/livewire/termsconditions.blade.php
@@ -11,18 +11,38 @@
                         <div class="overlay op7"></div>
                         <div class="container">
                             <div class="section-title center-align big-title">
-                                <h2><span>Our Tariff Plans</span></h2>
+                                <h2><span> Terms and Conditions</span></h2>
                                 <span class="section-separator"></span>
-                                <div class="breadcrumbs fl-wrap"><a href="/">Home</a><span>Pricing Tables</span></div>
+                                <div class="breadcrumbs fl-wrap"><a href="/">Home</a><span>Terms and Conditions</span></div>
                             </div>
                         </div>
                         <div class="header-sec-link">
-                            <a href="#sec1" class="custom-scroll-link"><i class="fal fa-angle-double-down"></i></a> 
+                            <a href="#sec1" class="custom-scroll-link"><i class="fal fa-angle-double-down"></i><h4>Term & Conditions</h4></a> 
                         </div>
                     </section>
                     <!--  section  end-->
                     <section   id="sec1" data-scrollax-parent="true">
-                        Terms and conditions
+                        
+                        <table class="table">
+  <thead class="thead-dark table table-bordered">
+    <tr>
+      <th scope="col">Sr.</th>
+      <th scope="col">T&C</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td> <b>DEFINITION CLAUSE:-</b>
+User: For purposes of this Agreement, a "User" is any person who accesses the Site for whatever purpose, <br>regardless of whether said User has registered with <b><span class="footer-logo-text"><strong style="color:#4DB7FE;">Trade</strong> <strong style="color:#4DB7FE;">My</strong> <strong style="color:#4DB7FE;">Deal</strong></b> as a registered User or whether said User is a paying customer for a specific service provided by <b><span class="footer-logo-text"><strong style="color:#4DB7FE;">Trade</strong> <strong style="color:#4DB7FE;">My</strong> <strong style="color:#4DB7FE;">Deal</strong></b>.<br> A User includes the person using this Site and any legal entity which may be represented by such person under actual or apparent authority.
+</td>
+  
+    </tr>
+ 
+  </tbody>
+</table>
+
+           
                     </section>
                 </div>
                 <!--content end-->
